bfef30c39b821258c5068955221c961af6f18b01
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-11-14 11:40:49 -0600

    added a series of prompts and confirmations in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+var generatePassword = function(){
+  console.log("clicked")
+  // input should be validated and at least one character type should be selected
+      // present the user with a series of prompts for password criteria
+      // a. select which criteria to include in the password
+      // b.choose a length of at least 8 characters and no more than 128 characters
+      // c.confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+      var passwordLength = parseInt(windom.prompt("What is your desired password length?"))
+      var includeLowerCase = window.confirm("Click OK if you would like lowercase letters in your password")
+      var includeUpperCase = window.confirm("Click OK if you would like uppercase letters in your password")
+      var includeNumbers = window.confirm("Click OK if you would like numbers in your password")
+      var includeSymbols = window.confirm("Click OK if you would like special characters in your password")
 
+  // password is generated that matches the selected criteria
+    // a. the password is either displayed in an alert or written to the page
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
